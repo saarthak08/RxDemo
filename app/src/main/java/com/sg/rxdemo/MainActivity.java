6c800e8219839200f769a6479deb024767e98aec
Saarthak <saarthakgupta08@gmail.com>
2019-05-01 07:06:47 +0530

    Added map & flatmap operator

--- app/src/main/java/com/sg/rxdemo/MainActivity.java
+++ app/src/main/java/com/sg/rxdemo/MainActivity.java
@@ -6,12 +6,18 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
@@ -26,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     private Observable<String[]> myStringObservable;
     private Observable<String> singleObservable;
     private Observable<Integer> integerObservable;
+    private final static String TAGS = "myApp";
+    private Observable<Student> myObservableStudent;
+    private DisposableObserver<Student> myObserverStudent;
     private CompositeDisposable compositeDisposable=new CompositeDisposable();
 
     private TextView textView;
@@ -49,11 +58,56 @@ public class MainActivity extends AppCompatActivity {
         integerObservable=Observable.range(1,20); //emit values ranging from 1 to 20
 
 
+        myObservableStudent=Observable.create(new ObservableOnSubscribe<Student>() {
+            @Override
+            public void subscribe(ObservableEmitter<Student> emitter) throws Exception {
+
+
+                ArrayList<Student> studentArrayList=getStudents();
+
+                for(Student student:studentArrayList){
+
+
+                    emitter.onNext(student);
+
+                }
+
+                emitter.onComplete();
+
+
+
+            }
+        });
+
+        compositeDisposable.add(
+                myObservableStudent
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        /*.map(new Function<Student, Student>() {
+                            @Override
+                            public Student apply(Student student) throws Exception {
+                                student.setEmail(student.getEmail().toUpperCase());
+                                return student;
+                            }
+                        })*/
+                        .flatMap(new Function<Student, ObservableSource<Student>>() {
+                            @Override
+                            public ObservableSource<Student> apply(Student student) throws Exception {
+                                Student student1=new Student();
+                                student1.setEmail(student.getEmail());
+                                Student student2=new Student();
+                                student2.setEmail("New Member: "+student.getEmail());
+                                return Observable.just(student,student1,student2);
+                            }
+                        })
+                        .subscribeWith(getObserver())
+        );
 
 
 
 
-       // myObservable.subscribeOn(Schedulers.io());
+
+        // myObservable.subscribeOn(Schedulers.io());
         //myObservable.observeOn(AndroidSchedulers.mainThread());
        /* myObserver=new Observer<String>() {
             @Override
@@ -149,6 +203,74 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         compositeDisposable.add(myStringObservable.subscribeWith(arrayObserver));
+    }
+
+
+    private DisposableObserver getObserver() {
+
+        myObserverStudent = new DisposableObserver<Student>() {
+            @Override
+            public void onNext(Student s) {
+
+
+                Log.i(TAGS, " onNext invoked with " + s.getEmail());
+            }
+
+            @Override
+            public void onError(Throwable e)
+
+            {
+                Log.i(TAGS, " onError invoked");
+            }
+
+            @Override
+            public void onComplete() {
+
+                Log.i(TAGS, " onComplete invoked");
+            }
+        };
+
+        return myObserverStudent;
+    }
+
+
+    private ArrayList<Student> getStudents() {
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        Student student1 = new Student();
+        student1.setName(" student 1");
+        student1.setEmail(" student1@gmail.com ");
+        student1.setAge(27);
+        students.add(student1);
+
+        Student student2 = new Student();
+        student2.setName(" student 2");
+        student2.setEmail(" student2@gmail.com ");
+        student2.setAge(20);
+        students.add(student2);
+
+        Student student3 = new Student();
+        student3.setName(" student 3");
+        student3.setEmail(" student3@gmail.com ");
+        student3.setAge(20);
+        students.add(student3);
+
+        Student student4 = new Student();
+        student4.setName(" student 4");
+        student4.setEmail(" student4@gmail.com ");
+        student4.setAge(20);
+        students.add(student4);
+
+        Student student5 = new Student();
+        student5.setName(" student 5");
+        student5.setEmail(" student5@gmail.com ");
+        student5.setAge(20);
+        students.add(student5);
+
+        return students;
+
+
     }
 
     @Override
